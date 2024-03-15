cf7c1215a73c46b055eeae6755fe0069d26d6af0
starkcodex <maneeshemn@gmail.com>
2024-03-15 19:55:36 +0530

    Integreted flask-wtf form.

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # create a flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'xtransparent zozilla'
+
+# create 
+class NamerForm(FlaskForm):
+    name = StringField('Please Enter Your Name:', validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -26,6 +35,20 @@ def page_not_found(e):
 def server_error(e):
     return render_template('500.html'), 505
 
+
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # validate form
+    if form.validate_on_submit():
+        name =form.name.data
+        form.name.data = ''
+        
+    return render_template('name.html', 
+                            name=name, 
+                            form=form
+                        )
 
 
 if __name__ == '__main__':
