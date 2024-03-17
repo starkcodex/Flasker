c78c96e1bf7b6b06fd0f5879b30f52686a36d69c
starkcodex <maneeshemn@gmail.com>
2024-03-17 10:56:40 +0530

    Initialization of eventapp project.

--- hello.py
+++ hello.py
@@ -1,21 +1,83 @@
-from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask import Flask, render_template, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 # create a flask app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/users_db'
 app.config['SECRET_KEY'] = 'xtransparent zozilla'
 
-# create 
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    
+    
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+# create a form for input 
 class NamerForm(FlaskForm):
     name = StringField('Please Enter Your Name:', validators=[DataRequired()])
     submit = SubmitField("Submit")
+    
+    
+    
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash('User updated Successfully.')
+            return render_template('update.html', 
+                                    form=form, 
+                                    name_to_update = name_to_update)
+        except:
+            flash('Error. something went wrong')
+            return render_template('update.html', form=form, name_to_update=name_to_update )
+    else:
+        return render_template('update.html', form=form, name_to_update=name_to_update)
+
+    
+    
+@app.route('/users/add', methods=['GET','POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User added Successfully')
+    our_users=Users.query.order_by(Users.date_added)
+    return render_template('add_user.html', form=form, name=name, our_users=our_users)
 
 
 @app.route('/')
 def index():
+    form = UserForm()
     first_name = "Mcodex"
     return render_template('index.html', first_name=first_name)
 
@@ -53,4 +115,6 @@ def name():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        # db.create_all()
+        app.run(debug=True)
